2fa71f8b9f6e97b63bd52451022162c2dbe30d38
wmoralesdev <walterrafael26@gmail.com>
2022-09-28 18:45:09 -0600

    Init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div className="w-screen h-screen flex justify-center items-center">
+        <h1 className="text-2xl text-white font-medium">Hello world!</h1>
+    </div>
+);
+
+export default App;
